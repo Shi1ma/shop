6779990a43641836c292f19a0261e55a9b8680db
aliakbarzohour <aliakbar.zohour@gmail.com>
2024-02-02 19:48:37 +0330

    undone project

---
+++ Back-End/login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require 'config/config.php';
+
+// define Email 
+$email = $_POST["email"];
+// define password 
+$password = $_POST["password"];
+// checking password for login 
+$password = sha1($password);
+$query = "select * from tbl_users where email = ? and password = ?";
+try {
+    $statement = $db->prepare($query);
+    // binding 
+    $statement->bind_param("ss", $email, $password);
+    // executing data 
+    $statement->execute();
+    // couting users in database 
+    $result = $statement->get_result();
+    if ($result->num_rows === 1) {
+        // define admin_id for database 
+        $row = $result->fetch_assoc();
+        $_SESSION["admin_id"] = $row["id"];
+        $_SESSION["admin_name"] = $row["name"];
+        $_SESSION["admin_email"] = $row["email"];
+        // headering after successful login 
+        header('location: ../Front-End/index.html');
+        // echo 'OK You LOGED IN ✅';
+    } else {
+        // header('Location: ../Front-End/login-page/index.html');
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+};
---
+++ Back-End/register.php
@@ -0,0 +1,40 @@
+<?php
+require 'config/config.php';
+
+
+session_start();
+if (isset($_SESSION["admin_id"])) {
+    header('location: done.html');
+}
+
+// define User name 
+$name = $_POST["user-name"];
+// define Email 
+$email = $_POST["email"];
+// define password 
+$password = $_POST["password"];
+// define repassword 
+$repassword = $_POST["repassword"];
+// checking password for login 
+if ($password === $repassword) {
+    $password = sha1($password);
+    $query = "insert into tbl_users (name, email, password) values (?,?,?)";
+    try {
+        $statement = $db->prepare($query);
+        // binding 
+        $statement->bind_param("sss", $name, $email, $password);
+        // executing data 
+        $statement->execute();
+        // and closing database 
+        $statement->close();
+        $db->close();
+        // routing in done page 
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    header('location: ../Front-End/login.html');
+    // you can use this code for testing 
+    // echo 'ok ✅ . your user name and password and email is define on database 👌🏼 .';
+} else {
+    echo 'passwod not match 🚫 . try agin';
+}
